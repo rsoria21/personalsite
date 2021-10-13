c9d9460088720bc44151f51caf8869911e2b2beb
rsoria21 <69863317+rsoria21@users.noreply.github.com>
2021-10-12 21:54:14 -0500

    text update

--- contactForm2/script.js
+++ contactForm2/script.js
@@ -9,21 +9,21 @@ $(document).ready(function () {
     statusElm.empty();
 
     if (email.length > 5 && email.includes("@") && email.includes(".")) {
-      statusElm.append("<div>Email is valid </div>");
+      statusElm.append("<div>Message recived</div>");
     } else {
       event.preventDefault();
       statusElm.append("<div>Email is not valid </div>");
     }
 
     if (subject.length >= 2) {
-      statusElm.append("<div>Subject is valid </div>");
+      statusElm.append("<div> </div>");
     } else {
       event.preventDefault();
       statusElm.append("<div>Subject is not valid </div>");
     }
 
     if (message.length >= 10) {
-      statusElm.append("<div>Message is valid </div>");
+      statusElm.append("<div>Talk to you soon </div>");
     } else {
       event.preventDefault();
       statusElm.append("<div>Message is not valid </div>");
